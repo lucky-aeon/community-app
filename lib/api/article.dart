81d90460aaee131b6ceea76f8823060aae6fa9ae
LiusCraft <liuscraft@qq.com>
2024-11-08 14:01:36 +0800

    feat: 文章列表查询结果展示

--- lib/api/article.dart
+++ lib/api/article.dart
@@ -20,7 +20,7 @@ class Article {
     }
   }
 
-Future<Result<article_model.Article?>> getDetail(int articleId) async {
+  Future<Result<article_model.Article?>> getDetail(int articleId) async {
     try {
       var response = await ApiBase.get('/articles/$articleId');
       if (!response['ok']) {
@@ -28,8 +28,29 @@ Future<Result<article_model.Article?>> getDetail(int articleId) async {
       }
       return Result<article_model.Article>(
           200, 'success', article_model.Article.fromJson(response['data']));
-  }catch (e) {
+    } catch (e) {
       return Result(500, e.toString(), null);
+    }
+  }
+
+  Future<Result<List<article_model.Article>>> getPageList(
+      int classifyId, int page, int pageSize, {String? searchTitle, int? userId}) async {
+    try {
+      var response = await ApiBase.get('/articles/list', params: {
+        'typeId': classifyId,
+        'page': page,
+        'limit': pageSize,
+      });
+      if (!response['ok']) {
+        throw Exception(response['msg']);
+      }
+      return Result<List<article_model.Article>>(
+          200,
+          'success',
+          List<article_model.Article>.from(
+              response['data']['list'].map((e) => article_model.Article.fromJson(e))));
+    } catch (e) {
+      return Result(500, e.toString(), []);
     }
   }
 }
--- lib/page/community/subcategory_tabview.dart
+++ lib/page/community/subcategory_tabview.dart
@@ -1,54 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:lucky_community/model/classify.dart';
+import 'package:lucky_community/page/community/article_list.dart';
 import 'package:lucky_community/provider/community.dart';
 import 'package:provider/provider.dart';
 
 class SubCategoryTabView extends StatefulWidget {
   final int parentId;
   final Widget child;
-  List<String> subCategories = [];
+  List<Classify> subCategories = [];
   SubCategoryTabView({super.key, required this.parentId, required this.child});
   @override
   State<SubCategoryTabView> createState() => _SubCategoryTabViewState();
 }
 
-class _SubCategoryTabViewState extends State<SubCategoryTabView> {
+class _SubCategoryTabViewState extends State<SubCategoryTabView>
+    with TickerProviderStateMixin {
   late CommunityProvider provider;
+  late TabController? _tabController;
 
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(length: widget.subCategories.length, vsync: this);
+    _tabController?.addListener(_handleTabSelection);
+
     provider = Provider.of<CommunityProvider>(context, listen: false);
     provider.getArticlesClassifys(widget.parentId).then((value) {
       setState(() {
-        widget.subCategories = value.map((e)=> e.title).toList();
-        debugPrint("subCategories: ${widget.subCategories}");
+        widget.subCategories = value.map((e) => e).toList();
+        _initializeTabController();
       });
     });
   }
 
   @override
+  void dispose() {
+    _tabController?.removeListener(_handleTabSelection);
+    _tabController?.dispose();
+    super.dispose();
+  }
+
+  void _initializeTabController() {
+    // 如果已有 TabController，先移除监听器并销毁它
+   _tabController?.removeListener(_handleTabSelection); // 使用 ! 强制解包
+    _tabController?.dispose();
+
+    // 初始化新的 TabController
+    _tabController = TabController(length: widget.subCategories.length, vsync: this);
+    _tabController?.addListener(_handleTabSelection);
+  }
+
+  void _handleTabSelection() {
+    if (_tabController!.indexIsChanging) {
+      provider.setCurrentClassify(widget.subCategories[_tabController!.index].id);
+      provider.getCurrentListDataByArticle();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: widget.subCategories.length,
-      child: Column(
-        children: [
-          TabBar(
-            isScrollable: true,
-            labelColor: Theme.of(context).primaryColor,
-            unselectedLabelColor: Colors.grey,
-            tabs: widget.subCategories
-                .map((subCategory) => Tab(text: subCategory))
-                .toList(),
-          ),
-          Expanded(
-            child: TabBarView(
-              children: widget.subCategories.map((subCategory) {
-                return Center(child: Text(subCategory));
-              }).toList(),
-            ),
-          ),
-        ],
-      ),
+    return Column(
+      children: [
+        TabBar(
+          controller: _tabController??TabController(length: widget.subCategories.length, vsync: this),
+          isScrollable: true,
+          labelColor: Theme.of(context).primaryColor,
+          unselectedLabelColor: Colors.grey,
+          tabs: widget.subCategories
+              .map((subCategory) => Tab(text: subCategory.title))
+              .toList(),
+        ),
+        Expanded(
+          child: widget.child,
+        ),
+      ],
     );
   }
 }
